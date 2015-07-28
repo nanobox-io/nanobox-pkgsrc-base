1efdc90cdf0fbac2b800c1713340d06f42ac2e75
Braxton Huggins <braxton@pagodabox.com>
2015-07-28 09:37:40 -0600

    update mysql 5.6 Makefile and PLIST to support somethings that may exit in Linux or Solaris

--- Vagrantfile
+++ Vagrantfile
@@ -53,7 +53,7 @@ Vagrant.configure('2') do |config|
 
   config.vm.provider 'virtualbox' do |v|
     v.memory = 4096
-    v.cpus   = 4
+    v.cpus   = 1
   end
 
   config.vm.provider "vmware_fusion" do |v, override|
@@ -64,42 +64,52 @@ Vagrant.configure('2') do |config|
     override.vm.box_url = 'https://github.com/pagodabox/vagrant-packer-templates/releases/download/v0.2.0/trusty64_vmware.box'
   end
 
-  # config.vm.network "private_network", type: "dhcp"
+  config.vm.network "private_network", type: "dhcp"
 
-  # config.vm.define "SmartOS" do |smartos|
-  #   smartos.vm.box = 'livinginthepast/smartos-base64'
-  #   smartos.vm.network "private_network", ip: "33.33.33.10"
-  #   smartos.vm.communicator = 'smartos'
-  #   smartos.ssh.insert_key = false
-  #   smartos.global_zone.platform_image = 'latest'
-  #   smartos.zone.name = 'base64'
-  #   smartos.zone.brand = 'joyent'
-  #   smartos.zone.image = '0edf00aa-0562-11e5-b92f-879647d45790'
-  #   smartos.zone.memory = 4096
-  #   smartos.zone.disk_size = 20
-  # end
+  config.vm.define "SmartOS" do |smartos|
+    smartos.vm.box = '/Users/braxton/src/pagodabox/vagrant-packer-templates/smartos-x86_64/packer_virtualbox-iso_virtualbox.box'
+    smartos.vm.communicator = 'smartos'
+    smartos.ssh.insert_key = false
+    smartos.global_zone.platform_image = 'latest'
+    smartos.zone.name = 'base64'
+    smartos.zone.brand = 'joyent'
+    smartos.zone.image = '0edf00aa-0562-11e5-b92f-879647d45790'
+    smartos.zone.memory = 4096
+    smartos.zone.disk_size = 20
+  end
 
   nanobox_user = ENV["NANOBOX_USER"] || 'nanobox'
   nanobox_project = ENV["NANOBOX_BASE_PROJECT"] || 'base'
   nanobox_secret = ENV["NANOBOX_BASE_SECRET"]
 
   # cache
-  config.vm.synced_folder "./.distfiles", "/content/distfiles"#, type: "nfs"
-  config.vm.synced_folder "./.packages", "/content/packages"#, type: "nfs"
+  config.vm.synced_folder "./.distfiles", "/content/distfiles"# , type: "nfs"
+  config.vm.synced_folder "./.packages", "/content/packages"# , type: "nfs"
 
   # pkgsrc framework
-  config.vm.synced_folder $pkgsrc, "/content/pkgsrc"#, type: "nfs"
+  config.vm.synced_folder $pkgsrc, "/content/pkgsrc"# , type: "nfs"
   # package definitions
-  config.vm.synced_folder ".", "/content/pkgsrc/base"#, type: "nfs"
+  config.vm.synced_folder ".", "/content/pkgsrc/base"# , type: "nfs"
 
   # utility scripts
-  config.vm.synced_folder "./.scripts", "/opt/util"#, type: "nfs"
+  config.vm.synced_folder "./.scripts", "/opt/util"# , type: "nfs"
 
   # ssh keys
-  config.vm.synced_folder "~/.ssh", "/var/.ssh"#, type: "nfs"
+  config.vm.synced_folder "~/.ssh", "/var/.ssh"# , type: "nfs"
 
   config.vm.provision "shell", inline: <<-SCRIPT
     echo "Preparing Environment..."
+    if [ ! -d /etc/profile.d ]; then
+      mkdir -p /etc/profile.d/
+      echo "if [ -d /etc/profile.d ]; then" >> /etc/profile
+      echo "  for i in /etc/profile.d/*.sh; do" >> /etc/profile
+      echo "    if [ -r \\$i ]; then" >> /etc/profile
+      echo "      . \\$i" >> /etc/profile
+      echo "    fi" >> /etc/profile
+      echo "  done" >> /etc/profile
+      echo "  unset i" >> /etc/profile
+      echo "fi" >> /etc/profile
+    fi
     echo # Vagrant environment variables > /etc/profile.d/vagrant.sh
     echo export PATH=/data/sbin:/data/bin:/data/gcc49/bin:/data/gnu/bin:$PATH >> /etc/profile.d/vagrant.sh
     echo export NANOBOX_USER=#{nanobox_user} >> /etc/profile.d/vagrant.sh
@@ -112,42 +122,43 @@ Vagrant.configure('2') do |config|
   config.vm.provision "shell", inline: <<-SCRIPT
     echo "installing build tools..."
     apt-get -y update -qq
+    apt-get -y upgrade
     apt-get install -y build-essential
   SCRIPT
 
-  # config.vm.provision 'shell', inline: <<-SCRIPT
-  #   echo "Downloading and Extracting Bootstrap..."
+  config.vm.provision 'shell', inline: <<-SCRIPT
+    echo "Downloading and Extracting Bootstrap..."
 
-  #   # detect platform
-  #   if [ $(uname | grep 'SunOS') ]; then
-  #     platform="SmartOS"
-  #   elif [ $(uname | grep 'Linux') ]; then
-  #     platform="Linux"
-  #   fi 
+    # detect platform
+    if [ $(uname | grep 'SunOS') ]; then
+      platform="SmartOS"
+    elif [ $(uname | grep 'Linux') ]; then
+      platform="Linux"
+    fi 
 
-  #   # download and extract the bootstrap
-  #   if [ ! -d /data ]; then
-  #     curl \
-  #       -s \
-  #       http://pkgsrc.nanobox.io/nanobox/base/${platform}/bootstrap.tar.gz \
-  #         | tar \
-  #             -xz \
-  #             -f - \
-  #             -C /
-  #   fi
+    # download and extract the bootstrap
+    if [ ! -d /data ]; then
+      curl \
+        -s \
+        http://pkgsrc.nanobox.io/nanobox/base/${platform}/bootstrap.tar.gz \
+          | tar \
+              -xz \
+              -f - \
+              -C /
+    fi
 
-  #   # ensure /data/var/db exists
-  #   if [ ! -d /data/var/db ]; then
-  #     mkdir -p /data/var/db
-  #   fi
-  # SCRIPT
+    # ensure /data/var/db exists
+    if [ ! -d /data/var/db ]; then
+      mkdir -p /data/var/db
+    fi
+  SCRIPT
 
-  # config.vm.provision 'shell', inline: <<-SCRIPT
-  #   echo "Install mksandbox utility..."
-  #   if [ ! -f /data/sbin/mksandbox ]; then
-  #     /data/bin/pkgin -y in mksandbox
-  #   fi
-  # SCRIPT
+  config.vm.provision 'shell', inline: <<-SCRIPT
+    echo "Install mksandbox utility..."
+    if [ ! -f /data/sbin/mksandbox ]; then
+      /data/bin/pkgin -y in mksandbox
+    fi
+  SCRIPT
 
   config.vm.provision 'shell', inline: <<-SCRIPT
     echo "Installing custom utilities..."
--- php/phpversion.mk
+++ php/phpversion.mk
@@ -12,7 +12,7 @@
 #	The PHP version to choose when more than one is acceptable to
 #	the package.
 #
-#	Possible: 53 54 55 56
+#	Possible: 53 54 55 56 70
 #	Default: 54
 #
 # === Infrastructure variables ===
@@ -29,15 +29,15 @@
 # PHP_VERSIONS_ACCEPTED
 #	The PHP versions that are accepted by the package.
 #
-#	Possible: 53 54 55 56
-#	Default: 54 53 55 56
+#	Possible: 53 54 55 56 70
+#	Default: 54 53 55 56 70
 #
 # === Variables defined by this file ===
 #
 # PKG_PHP_VERSION
 #	The selected PHP version.
 #
-#	Possible: 53 54 55 56
+#	Possible: 53 54 55 56 70
 #	Default: ${PHP_VERSION_DEFAULT}
 #
 # PHP_BASE_VERS
@@ -67,36 +67,40 @@
 #
 
 .if !defined(PHPVERSION_MK)
-PHPVERSION_MK=	defined
+PHPVERSION_MK=			defined
 
-_VARGROUPS+=	php
-_USER_VARS.php=	PHP_VERSION_DEFAULT
-_PKG_VARS.php=	PHP_VERSIONS_ACCEPTED PHP_VERSION_REQD
-_SYS_VARS.php=	PKG_PHP_VERSION PKG_PHP PHPPKGSRCDIR PHP_PKG_PREFIX \
-		PKG_PHP_MAJOR_VERS
+_VARGROUPS+=			php
+_USER_VARS.php=			PHP_VERSION_DEFAULT
+_PKG_VARS.php=			PHP_VERSIONS_ACCEPTED PHP_VERSION_REQD
+_SYS_VARS.php=			PKG_PHP_VERSION PKG_PHP PHPPKGSRCDIR PHP_PKG_PREFIX \
+				PKG_PHP_MAJOR_VERS
 
 .include "../../mk/bsd.prefs.mk"
 
 PHP53_VERSION=			5.3.29
-PHP54_VERSION=			5.4.42
-PHP55_VERSION=			5.5.26
-PHP56_VERSION=			5.6.10
+PHP54_VERSION=			5.4.43
+PHP55_VERSION=			5.5.27
+PHP56_VERSION=			5.6.11
+PHP70_VERSION=			7.0.0beta2
 
 PHP_VERSION_DEFAULT?=		54
-PHP_VERSIONS_ACCEPTED?=		54 53 55 56
+PHP_VERSIONS_ACCEPTED?=		54 53 55 56 70
 
 # transform the list into individual variables
 .for pv in ${PHP_VERSIONS_ACCEPTED}
-_PHP_VERSION_${pv}_OK=	yes
+_PHP_VERSION_${pv}_OK=		yes
 .endfor
 
 # check what is installed
-.if exists(${LOCALBASE}/lib/php/20131226)
-_PHP_VERSION_56_INSTALLED=  yes
-_PHP_INSTALLED=     yes
+.if exists(${LOCALBASE}/lib/php/20141001)
+_PHP_VERSION_70_INSTALLED=	yes
+_PHP_INSTALLED=			yes
+.elif exists(${LOCALBASE}/lib/php/20131226)
+_PHP_VERSION_56_INSTALLED=	yes
+_PHP_INSTALLED=			yes
 .elif exists(${LOCALBASE}/lib/php/20121212)
-_PHP_VERSION_55_INSTALLED=  yes
-_PHP_INSTALLED=     yes
+_PHP_VERSION_55_INSTALLED=	yes
+_PHP_INSTALLED=			yes
 .elif exists(${LOCALBASE}/lib/php/20100525)
 _PHP_VERSION_54_INSTALLED=	yes
 _PHP_INSTALLED=			yes
@@ -107,13 +111,13 @@ _PHP_INSTALLED=			yes
 
 # if a version is explicitely required, take it
 .if defined(PHP_VERSION_REQD)
-_PHP_VERSION=	${PHP_VERSION_REQD}
+_PHP_VERSION=			${PHP_VERSION_REQD}
 .endif
 # if the default is already installed, it is first choice
 .if !defined(_PHP_VERSION)
 .if defined(_PHP_VERSION_${PHP_VERSION_DEFAULT}_OK)
 .if defined(_PHP_VERSION_${PHP_VERSION_DEFAULT}_INSTALLED)
-_PHP_VERSION=	${PHP_VERSION_DEFAULT}
+_PHP_VERSION=			${PHP_VERSION_DEFAULT}
 .endif
 .endif
 .endif
@@ -121,7 +125,7 @@ _PHP_VERSION=	${PHP_VERSION_DEFAULT}
 .if !defined(_PHP_VERSION)
 .for pv in ${PHP_VERSIONS_ACCEPTED}
 .if defined(_PHP_VERSION_${pv}_INSTALLED)
-_PHP_VERSION?=	${pv}
+_PHP_VERSION?=			${pv}
 .else
 # keep information as last resort - see below
 _PHP_VERSION_FIRSTACCEPTED?=	${pv}
@@ -131,35 +135,39 @@ _PHP_VERSION_FIRSTACCEPTED?=	${pv}
 # if the default is OK for the addon pkg, take this
 .if !defined(_PHP_VERSION)
 .if defined(_PHP_VERSION_${PHP_VERSION_DEFAULT}_OK)
-_PHP_VERSION=	${PHP_VERSION_DEFAULT}
+_PHP_VERSION=			${PHP_VERSION_DEFAULT}
 .endif
 .endif
 # take the first one accepted by the package
 .if !defined(_PHP_VERSION)
-_PHP_VERSION=	${_PHP_VERSION_FIRSTACCEPTED}
+_PHP_VERSION=			${_PHP_VERSION_FIRSTACCEPTED}
 .endif
 
 #
 # Variable assignment for multi-PHP packages
-MULTI+=	PHP_VERSION_REQD=${_PHP_VERSION}
+MULTI+=	PHP_VERSION_REQD=	${_PHP_VERSION}
 
 # export some of internal variables
-PKG_PHP_VERSION:=	${_PHP_VERSION:C/\.[0-9]//}
-PKG_PHP:=		PHP${_PHP_VERSION:C/([0-9])([0-9])/\1.\2/}
+PKG_PHP_VERSION:=		${_PHP_VERSION:C/\.[0-9]//}
+PKG_PHP:=			PHP${_PHP_VERSION:C/([0-9])([0-9])/\1.\2/}
 
-# currently we have only PHP 5.x packages.
-PKG_PHP_MAJOR_VERS:=	5
+# currently only 7.0 is not a PHP 5.x packages.
+.if ${_PHP_VERSION} == "70"
+PKG_PHP_MAJOR_VERS:=		7
+.else
+PKG_PHP_MAJOR_VERS:=		5
+.endif
 
 # if installed PHP isn't compatible with required PHP, bail out
 .if defined(_PHP_INSTALLED) && !defined(_PHP_VERSION_${_PHP_VERSION}_INSTALLED)
-PKG_SKIP_REASON+=	"Package accepts ${PKG_PHP}, but different version is installed"
+PKG_SKIP_REASON+=		"Package accepts ${PKG_PHP}, but different version is installed"
 .endif
 
-MESSAGE_SUBST+=		PKG_PHP_VERSION=${PKG_PHP_VERSION} \
-			PKG_PHP=${PKG_PHP}
-PLIST_SUBST+=		PKG_PHP_VERSION=${PKG_PHP_VERSION} \
-			PKG_PHP_MAJOR_VERS=${PKG_PHP_MAJOR_VERS} \
-			PHP_EXTENSION_DIR=${PHP_EXTENSION_DIR}
+MESSAGE_SUBST+=			PKG_PHP_VERSION=${PKG_PHP_VERSION} \
+				PKG_PHP=${PKG_PHP}
+PLIST_SUBST+=			PKG_PHP_VERSION=${PKG_PHP_VERSION} \
+				PKG_PHP_MAJOR_VERS=${PKG_PHP_MAJOR_VERS} \
+				PHP_EXTENSION_DIR=${PHP_EXTENSION_DIR}
 
 # force the selected PHP version for recursive builds
 PHP_VERSION_REQD:=	${PKG_PHP_VERSION}
@@ -168,28 +176,33 @@ PHP_VERSION_REQD:=	${PKG_PHP_VERSION}
 # set variables for the version we decided to use:
 #
 .if ${_PHP_VERSION} == "53"
-PHPPKGSRCDIR=		../../base/php53
-PHP_BASE_VERS=		${PHP53_VERSION}
-PHP_NEXT_VERS=    5.4
-PHP_PKG_PREFIX=		php53
+PHPPKGSRCDIR=			../../base/php53
+PHP_BASE_VERS=			${PHP53_VERSION}
+PHP_NEXT_VERS=			5.4
+PHP_PKG_PREFIX=			php53
 .elif ${_PHP_VERSION} == "54"
-PHPPKGSRCDIR=		../../base/php54
-PHP_BASE_VERS=		${PHP54_VERSION}
-PHP_NEXT_VERS=    5.5
-PHP_PKG_PREFIX=		php54
+PHPPKGSRCDIR=			../../base/php54
+PHP_BASE_VERS=			${PHP54_VERSION}
+PHP_NEXT_VERS=			5.5
+PHP_PKG_PREFIX=			php54
 .elif ${_PHP_VERSION} == "55"
-PHPPKGSRCDIR=   ../../base/php55
-PHP_BASE_VERS=    ${PHP55_VERSION}
-PHP_NEXT_VERS=    5.6
-PHP_PKG_PREFIX=   php55
+PHPPKGSRCDIR=			../../base/php55
+PHP_BASE_VERS=			${PHP55_VERSION}
+PHP_NEXT_VERS=			5.6
+PHP_PKG_PREFIX=			php55
 .elif ${_PHP_VERSION} == "56"
-PHPPKGSRCDIR=   ../../base/php56
-PHP_BASE_VERS=    ${PHP56_VERSION}
-PHP_NEXT_VERS=    5.7
-PHP_PKG_PREFIX=   php56
+PHPPKGSRCDIR=			../../base/php56
+PHP_BASE_VERS=			${PHP56_VERSION}
+PHP_NEXT_VERS=			5.7
+PHP_PKG_PREFIX=			php56
+.elif ${_PHP_VERSION} == "70"
+PHPPKGSRCDIR=			../../base/php70
+PHP_BASE_VERS=			${PHP70_VERSION}
+PHP_NEXT_VERS=			7.1
+PHP_PKG_PREFIX=			php70
 .else
 # force an error
-PKG_SKIP_REASON+=	"${PKG_PHP} is not a valid package"
+PKG_SKIP_REASON+=		"${PKG_PHP} is not a valid package"
 .endif
 
 #
@@ -197,7 +210,7 @@ PKG_SKIP_REASON+=	"${PKG_PHP} is not a valid package"
 #
 .if defined(_PHP_VERSION_INSTALLED)
 .if ${_PHP_VERSION} != ${_PHP_VERSION_INSTALLED}
-PKG_SKIP_REASON+=	"${PKGBASE} requires ${PKG_PHP}, but php-${_PHP_VERSION_INSTALLED} is already installed."
+PKG_SKIP_REASON+=		"${PKGBASE} requires ${PKG_PHP}, but php-${_PHP_VERSION_INSTALLED} is already installed."
 .endif
 .endif
 

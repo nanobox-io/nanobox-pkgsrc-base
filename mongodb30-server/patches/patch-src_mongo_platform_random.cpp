3177a6fee7d53f621fc2eaaf936666bff50086cc
Braxton Huggins <braxton@pagodabox.com>
2015-12-02 16:07:30 -0700

    bump mongodb to version 3.0.7

--- mongodb30-server/patches/patch-src_mongo_platform_random.cpp
+++ mongodb30-server/patches/patch-src_mongo_platform_random.cpp
@@ -1,14 +1,13 @@
 $NetBSD$
 
-Add NetBSD support.
---- src/mongo/platform/random.cpp.orig	2015-04-08 20:28:08.000000000 +0000
+--- src/mongo/platform/random.cpp.orig	2015-10-12 20:46:18.000000000 +0000
 +++ src/mongo/platform/random.cpp
-@@ -117,7 +117,7 @@ namespace mongo {
-         return new WinSecureRandom();
-     }
+@@ -106,7 +106,7 @@ SecureRandom* SecureRandom::create() {
+     return new WinSecureRandom();
+ }
  
 -#elif defined(__linux__) || defined(__sunos__) || defined(__APPLE__) || defined(__freebsd__)
 +#elif defined(__linux__) || defined(__sunos__) || defined(__APPLE__) || defined(__freebsd__) || defined(__netbsd__)
  
-     class InputStreamSecureRandom : public SecureRandom {
-     public:
+ class InputStreamSecureRandom : public SecureRandom {
+ public:

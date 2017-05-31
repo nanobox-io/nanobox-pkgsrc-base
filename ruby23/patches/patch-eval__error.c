8ca6bb6670e94a88330119c9785ccbd1dc06e222
Braxton Huggins <braxton@pagodabox.com>
2017-05-31 09:06:33 -0600

    bump versions of ruby

--- ruby23/patches/patch-eval__error.c
+++ ruby23/patches/patch-eval__error.c
@@ -1,17 +1,15 @@
-$NetBSD: patch-eval__error.c,v 1.1 2015/12/30 14:59:42 taca Exp $
+$NetBSD$
 
---- eval_error.c.orig	2015-10-31 01:22:51.000000000 +0000
+--- eval_error.c.orig	2017-01-16 19:29:32.000000000 +0000
 +++ eval_error.c
-@@ -80,9 +80,9 @@ static void
+@@ -63,8 +63,8 @@ static void
  error_print(void)
  {
      volatile VALUE errat = Qundef;
 -    rb_thread_t *th = GET_THREAD();
 -    VALUE errinfo = th->errinfo;
--    int raised_flag = th->raised_flag;
-+    rb_thread_t * volatile th = GET_THREAD();
++    rb_thread_t volatile *th = GET_THREAD();
 +    volatile VALUE errinfo = th->errinfo;
-+    volatile int raised_flag = th->raised_flag;
+     volatile int raised_flag = th->raised_flag;
      volatile VALUE eclass = Qundef, e = Qundef;
      const char *volatile einfo;
-     volatile long elen;

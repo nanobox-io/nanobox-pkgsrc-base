100c27260f20c35d3ba860efafb6339ba1e0dccb
Braxton Huggins <braxton@pagodabox.com>
2017-05-31 09:02:39 -0600

    bump version of protobuf

---
+++ protobuf/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD: buildlink3.mk,v 1.3 2016/08/06 11:40:14 kamil Exp $
+
+BUILDLINK_TREE+=	protobuf
+
+.if !defined(PROTOBUF_BUILDLINK3_MK)
+PROTOBUF_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.protobuf+=	protobuf>=3.0.0
+BUILDLINK_PKGSRCDIR.protobuf?=	../../base/protobuf
+
+.include "../../devel/zlib/buildlink3.mk"
+.endif	# PROTOBUF_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-protobuf

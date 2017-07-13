096c2b977ecd7e0163bdf0a3ac6fee5f237a6a1d
Braxton Huggins <braxton@pagodabox.com>
2017-07-13 11:00:05 -0600

    add llvm 4.0.1

---
+++ llvm/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.2 2016/11/14 20:15:32 ryoon Exp $
+
+BUILDLINK_TREE+=	llvm
+
+.if !defined(LLVM_BUILDLINK3_MK)
+LLVM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.llvm+=	llvm>=4.0.0
+BUILDLINK_PKGSRCDIR.llvm?=	../../base/llvm
+
+LLVM_CONFIG_PATH?=		${BUILDLINK_PREFIX.llvm}/bin/llvm-config
+
+.include "../../devel/zlib/buildlink3.mk"
+.endif	# LLVM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-llvm

3e17f10c74bd2b6ee35a3c020539b85a2760717a
Braxton Huggins <braxton@pagodabox.com>
2017-07-13 11:04:13 -0600

    bump version of newrelic

--- newrelic/newrelicversion.mk
+++ newrelic/newrelicversion.mk
@@ -1,3 +1,3 @@
 .if !defined(NEWRELIC_VERSION)
-NEWRELIC_VERSION:=  7.2.0.191 # https://download.newrelic.com/php_agent/release/
+NEWRELIC_VERSION:=  7.4.0.198 # https://download.newrelic.com/php_agent/release/
 .endif

c9a79c4183f3e83f5e3c7fdcdb9637ca5c0bfd13
Braxton Huggins <braxton@nanobox.io>
2018-02-28 10:01:39 -0700

    add newrelic

--- newrelic/newrelicversion.mk
+++ newrelic/newrelicversion.mk
@@ -1,3 +1,3 @@
 .if !defined(NEWRELIC_VERSION)
-NEWRELIC_VERSION:=  7.6.0.201 # https://download.newrelic.com/php_agent/release/
+NEWRELIC_VERSION:=  7.7.0.203 # https://download.newrelic.com/php_agent/release/
 .endif
